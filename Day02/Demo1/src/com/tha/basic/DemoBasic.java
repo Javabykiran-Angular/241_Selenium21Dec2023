250a11a101e907c7ed5a87a65a61f4e0ae4d2459
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2023-12-22 10:16:19 +0530

    Day02

---
+++ Day02/Demo1/src/com/tha/basic/DemoBasic.java
@@ -0,0 +1,22 @@
+package com.tha.basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoBasic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		
+		WebDriver  driver=new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		
+		
+		
+		
+
+	}
+
+}
