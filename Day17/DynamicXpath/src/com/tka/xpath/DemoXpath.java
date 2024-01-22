5f8cbd99a1a3473f2c2c169cb172f4a99ce5eb59
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-01-22 10:04:39 +0530

    Day17

---
+++ Day17/DynamicXpath/src/com/tka/xpath/DemoXpath.java
@@ -0,0 +1,18 @@
+package com.tka.xpath;
+
+import com.tka.dynamicXpath.Config;
+
+public class DemoXpath extends Config {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		DemoXpath obj=new DemoXpath();
+//		obj.browserSetting("https://www.w3schools.com/");
+		obj.browserSetting("https://register.rediff.com/register/register.php?FormName=user_details");
+//		obj.accessElement();
+		obj.rediffElement();
+
+	}
+
+}
