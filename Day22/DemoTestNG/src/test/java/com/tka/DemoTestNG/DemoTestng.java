658f34affa3c3e9f1f33feb6c18af6d23417fff1
The Kiran Academy <71135924+Javabykiran-Angular@users.noreply.github.com>
2024-02-01 10:18:11 +0530

    Day22

---
+++ Day22/DemoTestNG/src/test/java/com/tka/DemoTestNG/DemoTestng.java
@@ -0,0 +1,13 @@
+package com.tka.DemoTestNG;
+
+import org.testng.annotations.Test;
+
+public class DemoTestng {
+	
+	@Test
+	public void loginTest() {
+		System.out.println(".....U r in loginTest........");
+	}
+	
+
+}
